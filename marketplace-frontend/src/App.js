166f219a543bf5e92e943757e92b5650a2e752e6
دابس بالعربي <arabicdapps@gmail.com>
2024-08-12 19:45:21 +0300

    s

--- marketplace-frontend/src/App.js
+++ marketplace-frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import NavigationBar from "./components/NavigationBar";
 import ProductList from "./components/ProductList";
 import CreateProduct from "./components/CreateProduct";
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <div>
-        <Navbar />
+        <NavigationBar />
         <Routes>
           <Route path="/" element={<ProductList />} />
           <Route path="/create" element={<CreateProduct />} />
